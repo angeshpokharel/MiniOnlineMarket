f3f090a4588304a383043a92d7ceba1f60f0689d
rabinsen <rivansen0071@gmail.com>
2021-12-09 15:10:14 -0600

    Revert "Merge branch 'Angesh' into Rabin"
    This reverts commit dfe27ce6b147c1f23b9c28bdf156ab4152abba82, reversing
    changes made to 0038b52fe3a131bc80fe26a59c3ed93034812d17.

--- backend/src/main/java/com/waa/project/domain/Role.java
+++ backend/src/main/java/com/waa/project/domain/Role.java
@@ -2,7 +2,10 @@ package com.waa.project.domain;
 
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
@@ -14,7 +17,6 @@ public class Role {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
+
     private String name;
-    @ManyToOne(cascade = CascadeType.ALL)
-    private User user;
 }
