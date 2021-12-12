0186edd6355f2cbbf754fb1c808879c53c9904f9
Win-Ei-Khaing <wineikhaing111111@gmail.com>
2021-12-12 12:34:40 -0600

    deleted unworking mom app

--- mom/src/pages/public/not-authorized/NotAuthorized.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const NotAuthorized = () => <h1>NOT AUTHORIZED</h1>;
-
-export default NotAuthorized;
--- mom/src/pages/public/not-found/GlobalPageNotFound.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const GlobalPageNotFound = () => <h1>PAGE NOT FOUND</h1>
-
-export default GlobalPageNotFound;
