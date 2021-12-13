93f2fdf1eb2822d43c2391f7dbb32db154c26323
Win-Ei-Khaing <wineikhaing111111@gmail.com>
2021-12-13 15:28:50 -0600

    seller listing to follow

--- mom/src/pages/buyer/component/follower/Follower.jsx
+++ mom/src/pages/buyer/component/follower/Follower.jsx
@@ -1,12 +1,67 @@
-import React from "react";
+import { TableCell, TableRow } from "@material-ui/core";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Table } from "react-bootstrap";
+import MOM, { API_URL } from "../../../../api/api";
 import BuyerHeader from "../common/BuyerHeader";
+import './Follower.css';
 
 function Follwer(){
-    return (
-        <div>
-          <BuyerHeader name="Follower"/>
-          <h1>I'm buyer follower</h1>
-        </div>
+  //state to control effect - win
+  const [refresh, setRefresh] = useState(false);
+
+  const [sellers, setSellers] = useState([]); 
+
+  const getSellers = ()=>{
+                        MOM.get(API_URL.user) 
+                          .then((response) => {
+                              const data = response.data;
+                              //got all users. so filtered with role ROLE_SELLER
+                              var result = data.filter(user => user.role === 'ROLE_SELLER');
+                              setSellers(result);
+                          })
+                          .catch(error=>console.log("Retrieving sellers was failed : " + error.message))
+                      }
+
+  useEffect(() => {
+    getSellers();
+  }, [refresh]);  
+
+  return (
+    <div>
+      <BuyerHeader name="Follower"/>
+      <section className="Sellers">
+            <h3>List of sellers in the online store</h3> 
+            <Table striped bordered hover className="SellerTable">
+                <thead>
+                    <tr>
+                        <th className="thSellerID">Seller ID </th>
+                        <th className="thName">Name</th>
+                        <th className="thEmail">Email</th>
+                        <th className="thAddress">Address</th>
+                        <th className="thPhone">Phone</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                  {
+                    sellers.map(seller => {
+                      return (
+                        <TableRow key={seller.id}>
+                          <TableCell>{seller.id}</TableCell>
+                          <TableCell>{seller.name}</TableCell>
+                          <TableCell>{seller.email}</TableCell>
+                          <TableCell>{seller.address}</TableCell>
+                          <TableCell>{seller.phone}</TableCell>
+                          <TableCell><button id="btnFollow"> Follow</button></TableCell>
+                      </TableRow>
+                      );
+                    })
+                  }
+                </tbody>
+            </Table>
+        </section>
+    </div>
     )
 }
 
